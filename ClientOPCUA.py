b038966538d8a8c5bcb6a5a9f76c3b0299343ba7
DianaCalugaru <131282859+DianaCalugaru@users.noreply.github.com>
2023-06-03 23:36:48 +0300

    Script Functional
    Sistemul de alarma- sound si switch functional

--- ClientOPCUA.py
+++ ClientOPCUA.py
@@ -4,6 +4,17 @@ import pyodbc as pyodbc
 from opcua import Client
 import time
 
+# Variabile pentru a memora valorile anterioare
+prev_led1_state = None
+prev_led2_state = None
+prev_led3_state = None
+prev_led5_state = None
+prev_led6_state = None
+prev_hum_state = None
+prev_temp_state = None
+prev_flame_state = None
+prev_sound_state = None
+
 url = "opc.tcp://169.254.15.16:4840"
 
 client = Client(url)
@@ -14,78 +25,146 @@ print("Client Connected")
 # Definirea datelor de conectare la baza de date
 server = 'DESKTOP-8QAOT1N\SQLEXPRESS'
 database = 'IoT'
-driver = '{SQL Server}' # driver-ul de conectare
+driver = '{SQL Server}'  # driver-ul de conectare
 
 # Conectarea la baza de date
-conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
+conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';Trusted_Connection=yes;')
 cursor = conn.cursor()
 
 
+def insert_led_state(id, name, state):
+    insert_query_led = f"INSERT INTO Table_LED (ID, Nume, Stare, Data) VALUES ({id}, '{name}', '{state}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
+    cursor.execute(insert_query_led)
+
+
+def insert_dht11_values(id, name, value):
+    insert_query_dht11 = f"INSERT INTO Table_DHT11 (ID, Nume, Valoare, Data) VALUES ({id}, '{name}', '{value}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
+    cursor.execute(insert_query_dht11)
+
+
+def insert_flame_value(name, value):
+    insert_query_flame = f"INSERT INTO Table_Flame (Nume, Valoare, Data) VALUES ('{name}', '{value}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
+    cursor.execute(insert_query_flame)
+
+def insert_sound_value(name, value):
+    insert_query_flame = f"INSERT INTO Table_Sound (Nume, Valoare, Data) VALUES ('{name}', '{value}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
+    cursor.execute(insert_query_flame)
+
 while True:
-    led1 = client.get_node("ns=2;i=2")
+    led1 = client.get_node("ns=2;i=5")
     Led1 = led1.get_value()
     print(Led1)
 
-    led2 = client.get_node("ns=2;i=3")
+    led2 = client.get_node("ns=2;i=6")
     Led2 = led2.get_value()
     print(Led2)
 
-    led3 = client.get_node("ns=2;i=4")
+    led3 = client.get_node("ns=2;i=7")
     Led3 = led3.get_value()
     print(Led3)
 
-    led5 = client.get_node("ns=2;i=5")
+    led5 = client.get_node("ns=2;i=8")
     Led5 = led5.get_value()
     print(Led5)
 
-    led6 = client.get_node("ns=2;i=6")
+    led6 = client.get_node("ns=2;i=9")
     Led6 = led6.get_value()
     print(Led6)
 
-    # modificare pentru tabela Table_LED
-    if Led1 == 1:
-        led1_state = 'Aprins'
-    else:
-        led1_state = 'Stins'
+    if Led1 != prev_led1_state:
+        if Led1 == 1:
+            led1_state = 'Aprins'
+        else:
+            led1_state = 'Stins'
+        insert_led_state(1, 'LED1', led1_state)
 
-    if Led2 == 1:
-        led2_state = 'Aprins'
-    else:
-        led2_state = 'Stins'
+    if Led2 != prev_led2_state:
+        if Led2 == 1:
+            led2_state = 'Aprins'
+        else:
+            led2_state = 'Stins'
+        insert_led_state(2, 'LED2', led2_state)
 
-    if Led3 == 1:
-        led3_state = 'Aprins'
-    else:
-        led3_state = 'Stins'
+    if Led3 != prev_led3_state:
+        if Led3 == 1:
+            led3_state = 'Aprins'
+        else:
+            led3_state = 'Stins'
+        insert_led_state(3, 'LED3', led3_state)
 
-    if Led5 == 1:
-        led5_state = 'Aprins'
-    else:
-        led5_state = 'Stins'
+    if Led5 != prev_led5_state:
+        if Led5 == 1:
+            led5_state = 'Aprins'
+        else:
+            led5_state = 'Stins'
+        insert_led_state(4, 'LED5', led5_state)
 
-    if Led6 == 1:
-        led6_state = 'Aprins'
-    else:
-        led6_state = 'Stins'
+    if Led6 != prev_led6_state:
+        if Led6 == 1:
+            led6_state = 'Aprins'
+        else:
+            led6_state = 'Stins'
+        insert_led_state(5, 'LED6', led6_state)
 
-    insert_query_led = f"INSERT INTO Table_LED (ID, Nume, Stare, Data) VALUES (1, 'LED1', '{led1_state}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}'), (2, 'LED2', '{led2_state}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}'), (3, 'LED3', '{led3_state}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}'), (4, 'LED5', '{led5_state}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}'), (5, 'LED6', '{led6_state}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
-    cursor.execute(insert_query_led)
 
-    hum = client.get_node("ns=2;i=9")
+    hum = client.get_node("ns=2;i=10")
     Hum6 = hum.get_value()
     print(Hum6)
 
-    temp = client.get_node("ns=2;i=8")
+    temp = client.get_node("ns=2;i=11")
     Temp = temp.get_value()
     print(Temp)
 
-    insert_query_dht11 = f"INSERT INTO Table_DHT11 (ID, Nume, Valoare, Data) VALUES (1, 'Umiditate', '{Hum6}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}'), (2, 'Temperatura', '{Temp}', '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
-    cursor.execute(insert_query_dht11)
+    if Hum6 != prev_hum_state:
+        insert_dht11_values(1, 'Umiditate', Hum6)
+
+    if Temp != prev_temp_state:
+        insert_dht11_values(2, 'Temperatura', Temp)
+
+
+    flame = client.get_node("ns=2;i=12")
+    Flame = flame.get_value()
+    print(Flame)
+
+    if Flame == 1:
+        Flame_state = 'Detectat'
+        print(Flame_state)
+    else:
+        Flame_state = 'Nedetectat'
+        print(Flame_state)
+
+    if Flame != prev_flame_state:
+        insert_flame_value('Flame', Flame_state)
+
+     #////////////
+    sound = client.get_node("ns=2;i=13")
+    Sound = sound.get_value()
+    print(Sound)
+
+    if Sound == 1:
+        Sound_state = 'Detectat'
+        print(Sound_state)
+    else:
+        #Sound_state = 'Nedetectat'
+        print(Sound_state)
+
+    if Sound != prev_sound_state:
+        insert_sound_value('Sound', Sound_state)
+
+
+    # Update previous states
+    prev_led1_state = Led1
+    prev_led2_state = Led2
+    prev_led3_state = Led3
+    prev_led5_state = Led5
+    prev_led6_state = Led6
+    prev_hum_state = Hum6
+    prev_temp_state = Temp
+    prev_flame_state = Flame
+    prev_sound_state = Sound
 
     time.sleep(2)
     conn.commit()
-
-
 
 # Închiderea conexiunii cu baza de date
 cursor.close()
--- ServerVar3.py
+++ ServerVar3.py
@@ -1,9 +1,6 @@
-
 from opcua import Server
 import RPi.GPIO as GPIO
 import Adafruit_DHT
-from random import randint
-import datetime
 import time
 
 DHT_SENSOR = Adafruit_DHT.DHT11
@@ -14,7 +11,8 @@ LED2_PIN = 17
 LED3_PIN = 26
 LED4_PIN = 13
 LED5_PIN = 19
-
+ALARM_PIN = 12
+SWITCH_PIN = 23
 
 server = Server()
 url = "opc.tcp://169.254.15.16:4840"
@@ -26,31 +24,41 @@ addspace = server.register_namespace(name)
 node = server.get_objects_node()
 
 Led = node.add_object(addspace, "Led")
+DHT = node.add_object(addspace, "DHT")
+FlameSensor = node.add_object(addspace, "FlameSensor")
+SoundSensor = node.add_object(addspace, "SoundSensor")
+Switch = node.add_object(addspace, "Switch")
+
+
 Led1 = Led.add_variable(addspace, "Led1", 0)
 Led2 = Led.add_variable(addspace, "Led2", 0)
 Led3 = Led.add_variable(addspace, "Led3", 0)
 Led4 = Led.add_variable(addspace, "Led4", 0)
 Led5 = Led.add_variable(addspace, "Led5", 0)
-
-DHT = node.add_object(addspace, "DHT")
 DHT_temperature = DHT.add_variable(addspace, "temperature", 0)
 DHT_humidity = DHT.add_variable(addspace, "humidity", 0)
+FlameValue = FlameSensor.add_variable(addspace, "FlameValue", 0)
+SoundValue = SoundSensor.add_variable(addspace, "SoundValue", 0)
+SwitchValue = Switch.add_variable(addspace, "Switch", 0)
 
 Led1.set_writable()
 Led2.set_writable()
 Led3.set_writable()
 Led4.set_writable()
 Led5.set_writable()
-
+FlameValue.set_writable()
+SoundValue.set_writable()
+SwitchValue.set_writable()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FLAME_SENSOR_PIN, GPIO.IN)
+GPIO.setup(ALARM_PIN, GPIO.IN)
 GPIO.setup(LED1_PIN, GPIO.OUT)
 GPIO.setup(LED2_PIN, GPIO.OUT)
 GPIO.setup(LED3_PIN, GPIO.OUT)
 GPIO.setup(LED4_PIN, GPIO.OUT)
 GPIO.setup(LED5_PIN, GPIO.OUT)
-
+GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 server.start()
 
@@ -60,8 +68,6 @@ def FunctionLedOn(Led, Pin):
     print(LedVar)
     Led.set_value(LedVar)
 
-
-    
 def FunctionLedOff(Led, Pin):
     GPIO.output(Pin, GPIO.LOW)
     LedVar = 0
@@ -73,44 +79,70 @@ def FunctionLedOnOff(Led, Pin):
     LedVar = 1
     print(LedVar)
     Led.set_value(LedVar)
-    time.sleep(4)
+    time.sleep(2)
+   
     GPIO.output(Pin, GPIO.LOW)
     LedVar = 0
     print(LedVar)
     Led.set_value(LedVar)
-    time.sleep(4)
+
 def FunctionFlameSenzor(FLAME_SENSOR_PIN):
     if GPIO.input(FLAME_SENSOR_PIN):
-        print("Nicio flacara detectata")
+        print("Nicio flacară detectată")
+        FlameValue.set_value(0)
     else:
-        print("Flacara detectata!")
+        print("Flacară detectată!")
+        FlameValue.set_value(1)
 
-    # Asteapta 0.1 secunde pana la urmatoarea citire
-    time.sleep(0.1)
-    
 def FunctionDHT(DHT_SENSOR, DHT_PIN):
-	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN);
-	if humidity is not None and temperature is not None:
-		DHT_temperature.set_value(temperature);
-		DHT_humidity.set_value(humidity);
-		print("Temperature: {:.2f} C, Humidity: {:.2f} %".format(temperature, humidity))
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        DHT_temperature.set_value(temperature)
+        DHT_humidity.set_value(humidity)
+        print("Temperature: {:.2f} C, Humidity: {:.2f} %".format(temperature, humidity))
+    else:
+        print("Failed to read data from DHT11 sensor")
+
+
+# Funcție pentru tratarea evenimentului de detectare a sunetului
+def handle_sound(channel):
+    if GPIO.input(ALARM_PIN):
+        print("Sunet detectat!")
+        SoundValue.set_value(1)  # Setare valoare OPC UA la 1
+        time.sleep(1)
+    else:
+        SoundValue.set_value(0)  # Setare valoare OPC UA la 0
+ 
+def handle_switch(channel):
+	if GPIO.input(SWITCH_PIN) == GPIO.LOW:
+		print('Button Pressed')
+		SwitchValue.set_value(1)
 	else:
-		print("Failed to read data from DHT11 sensor")
-	time.sleep(1)  # Așteaptă 5 secunde înainte de a citi din nou valorile
-    
+		SwitchValue.set_value(0)
+		
+              
+# Adăugare eveniment pentru detectarea modificărilor pe pinul senzorului de sunet
+GPIO.add_event_detect(ALARM_PIN, GPIO.BOTH, callback=handle_sound)
+
+
+# Adăugare eveniment pentru detectarea modificărilor pe pinul de comutare
+GPIO.add_event_detect(SWITCH_PIN, GPIO.BOTH, callback=handle_switch)
+
 
 
 print("Server started at {}".format(url))
 
 try:
     while True:
-        FunctionLedOnOff(Led1, LED1_PIN);
-        FunctionLedOnOff(Led2, LED2_PIN);
-        FunctionLedOn(Led3, LED3_PIN);
-        FunctionLedOn(Led4, LED4_PIN);
-        FunctionLedOn(Led5, LED5_PIN);
-        FunctionDHT(DHT_SENSOR, DHT_PIN);
+        FunctionLedOn(Led1, LED1_PIN)
+        FunctionLedOn(Led2, LED2_PIN)
+        FunctionLedOn(Led3, LED3_PIN)
+        FunctionLedOn(Led4, LED4_PIN)
+        FunctionLedOnOff(Led5, LED5_PIN)
+        FunctionDHT(DHT_SENSOR, DHT_PIN)
         FunctionFlameSenzor(FLAME_SENSOR_PIN)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     GPIO.cleanup()
+
 
